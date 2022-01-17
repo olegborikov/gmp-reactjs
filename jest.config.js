0e11a2c6c081a9a79c92427d8d54ab2dac2944da
Oleg Borikov <olegborikov999@gmail.com>
2022-01-17 20:53:13 +0300

    improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,36 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
   testEnvironment: "jsdom",
-  moduleNameMapper: {'^.+\\.(css|less|scss)$': 'identity-obj-proxy'},
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts"
+  ],
+  resolver: "jest-pnp-resolver",
+  testMatch: [
+    '<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  testURL: "http://localhost",
+  testPathIgnorePatterns: [
+    "node_modules"
+  ],
+  modulePathIgnorePatterns: [
+    "/build/"
+  ],
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+  },
+  moduleFileExtensions: [
+    "web.js",
+    "js",
+    "web.ts",
+    "ts",
+    "web.tsx",
+    "tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node"
+  ]
 }
